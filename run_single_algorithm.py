0fb9289644b05de64d05a246bd6e0ccc7842ad95
Godspeeeeeedddddd <78198782+IoET-y@users.noreply.github.com>
2025-03-06 23:22:32 -0500

    Update run_single_algorithm.py

--- run_single_algorithm.py
+++ run_single_algorithm.py
@@ -7,11 +7,11 @@ import seaborn as sns
 import holoocean
 from tqdm import tqdm
 from auv_planning.planning.ASTAR_2025 import AStarPlanner
-from auv_planning.planning.DJS_2025 import DijkstraPlanner
 from auv_planning.planning.RRT_2025 import RRTPlanner
+from auv_planning.planning.DJS_2025 import DijkstraPlanner
 from auv_planning.planning.GA_2025 import GeneticPlanner
-from auv_planning.planning.FA_2025 import FireflyPlanner
 from auv_planning.planning.ACO_2025 import ACOPlanner
+from auv_planning.planning.FA_2025 import FireflyPlanner
 
 from auv_planning.planning.RTASTART_2025 import RTAAStarPlanner
 from auv_planning.planning.RRTOL_2025 import OnlineRRTStarPlanner
@@ -20,7 +20,6 @@ from auv_planning.planning.Theta_2025 import RSAPPlanner
 from auv_planning.planning.SAC_2025 import SACPlanner
 from auv_planning.planning.SAC_LQR_20251 import SACLQRPlanner
 
-from plotter import Plotter
 from auv_planning.holoocean_config import scenario
 from auv_planning.custom_env import custom_environment
 
@@ -28,16 +27,16 @@ sns.set(context="paper", style="whitegrid", font_scale=0.8)
 
 
 def main(show, plot, verbose, route, num_episodes, max_step, model_path, BS):
-    # 检查 holoocean 环境是否安装
+#check if need to install holoocean
     if "Ocean" not in holoocean.installed_packages():
         holoocean.install("Ocean")
 
-    # 初始化仿真环境
+    # initialize holoocean
     print("Starting HoloOcean environment initialization...")
     dummy_env = custom_environment(scenario_cfg=scenario, n_targets=10, n_obstacles=20, show_viewport=show, verbose=verbose)
     print("Environment initialized successfully.")
 
-    # 初始化控制器和规划器
+    # initialize planner
     planner = None
     if route == "rrt":
         planner = RRTPlanner(grid_resolution=0.5, max_steps=max_step)
@@ -59,16 +58,18 @@ def main(show, plot, verbose, route, num_episodes, max_step, model_path, BS):
     elif route == "sac":
         planner = SACPlanner(batch_size=BS, config_file="config_all.yaml")
         planner.train(dummy_env, model_path=model_path)
-    else:
-        planner = Traj(route, num_episodes)
-
+    elif route == "PFM":
+        planner = PFMPlanner(grid_resolution=0.5, max_steps=max_step)
+    elif route == "RTTOL":
+        planner = OnlineRRTStarPlanner(grid_resolution=0.5, max_steps=max_step)
+    elif route == "RTAST":
+        planner = RTAAStarPlanner(grid_resolution=0.5, max_steps=max_step)
+    elif route == "RSAP":
+        planner = RSAPPlanner(grid_resolution=0.5, max_steps=max_step)
     if planner is not None:
         planner.train(dummy_env)
         print(f"Using {route} planner.")
 
-    # 如果需要绘图
-    if plot:
-        plotter = Plotter(["True", "Estimated", "Desired"])
 
 
 if __name__ == "__main__":
@@ -84,16 +85,16 @@ if __name__ == "__main__":
 
 
     parser = argparse.ArgumentParser()
-    parser.add_argument("--route", type=str, default="ACO", help="选择使用的规划算法")
-    parser.add_argument("--num_episodes", type=int, default=5000, help="训练的总回合数")
-    parser.add_argument("--max_step", type=int, default=10000, help="每回合最大步数")
-    parser.add_argument("--batch_size", type=int, default=32, help="批量大小")
-    parser.add_argument("--model_path", type=str, default="as.pth", help="模型存储路径")
-    parser.add_argument("--show", action="store_true", help="是否显示仿真窗口")
-    parser.add_argument("--plot", action="store_true", help="是否显示绘图")
-    parser.add_argument("--verbose", action="store_true", help="是否启用详细日志")
-    parser.add_argument("--n_targets", type=int, default=10, help="目标物数量")
-    parser.add_argument("--n_obstacles", type=int, default=20, help="障碍物数量")
+    parser.add_argument("--route", type=str, default="RSAP", help="select planner")
+    parser.add_argument("--num_episodes", type=int, default=5000, help="total runing episode")
+    parser.add_argument("--max_step", type=int, default=10000, help="maxstep per episode")
+    parser.add_argument("--batch_size", type=int, default=32, help="Batch size for reinforcement learning")
+    parser.add_argument("--model_path", type=str, default="as.pth", help="RL model storage path")
+    parser.add_argument("--show", action="store_true", help="show simulation windows")
+    parser.add_argument("--plot", action="store_true", help="plot or noit")
+    parser.add_argument("--verbose", action="store_true", help="Log or not")
+    parser.add_argument("--n_targets", type=int, default=10, help="Targets number")
+    parser.add_argument("--n_obstacles", type=int, default=20, help="Obstacles number")
 
     args = parser.parse_args()
 
